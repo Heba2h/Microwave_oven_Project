3c1ad0f33afd194a8017a2b8bd0dcd49f66645fc
Eyad Elkiey <eyadelkiey@hotmail.com>
2022-05-18 00:15:03 +0200

    Update control.h

--- control.h
+++ control.h
@@ -1,5 +1,5 @@
- #ifndef CONTROL_H
- #define CONTROL_H
+#ifndef CONTROL_H
+#define CONTROL_H
  
 #include "tm4c123gh6pm.h"
 #include "delay.h" 
@@ -16,12 +16,16 @@
 
  } 
 
-  void Buzzer_toggle(){ // toggling the buzzer
+ void Buzzer_ON(){ // toggling the buzzer
 
-	   GPIO_PORTA_DATA_R ^= 0x01; 
+	   GPIO_PORTD_DATA_R |= 0x01; 
 
  }
+void Buzzer_OFF(){ // toggling the buzzer
 
+	   GPIO_PORTD_DATA_R &= ~0x01;
+
+ }
 	void Led_Blinking(){   //toggling the leds
 
 	GPIO_PORTF_DATA_R ^= 0x0E; 
@@ -30,19 +34,26 @@
 
  void end_of_Operation(){  // should be called when countdown is equal to zero (end Case ) ( if     ((NVIC_ST_CTRL_R & 0x000100000) == 0x01);  ))))))
 
-	 int i;
+		int i;
 
-
-for(i =0;i<=5;i++){ 
+	for(i =0;i<=5;i++){ 
 
 		Led_Blinking();
-
-		Buzzer_toggle(); // el mafrood tb2a enable*****
-
 		Systick_ms(500);
-
  }
-
 } 
+ void buzzer_init(void){
+	SYSCTL_RCGCGPIO_R |= 0x08;
+	while ((SYSCTL_PRGPIO_R &0x08)==0);
+	
+	GPIO_PORTD_LOCK_R = 0x4C4F434B;
+	GPIO_PORTD_AMSEL_R &=~ 0x01;
+	GPIO_PORTD_CR_R |= 0x01;
+	GPIO_PORTD_PCTL_R &=~ 0x0000000F;
+	GPIO_PORTD_AFSEL_R &=~ 0x01;
+	GPIO_PORTD_DIR_R |= 0x01;
+	GPIO_PORTD_DEN_R |= 0x01;
+	GPIO_PORTD_DATA_R &=~ 0x01;
+}
 
 #endif
