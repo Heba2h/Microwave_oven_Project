2a497f3e2f1b3f9bddcca4126a9d64b799c17940
Aser-ElDahshan <104104241+Aser-ElDahshan@users.noreply.github.com>
2022-05-25 01:30:09 +0200

    documentation

--- delay.h
+++ delay.h
@@ -2,6 +2,12 @@
 #define delay_H
 #include "tm4c123gh6pm.h"
 
+
+/**
+*  @author      Aser-ElDahshan
+*  @detailed    This fuction initializes Systick.
+*/
+
 void SysTick_Init(void)
 {
 	NVIC_ST_CTRL_R = 0;
@@ -9,22 +15,33 @@ void SysTick_Init(void)
 	NVIC_ST_CURRENT_R = 0;
 	NVIC_ST_CTRL_R = 5;
 }
+
+/**
+*  @author      Aser-ElDahshan
+*  @detailed    This fuction gives the reload register a value to count down from it waiting untill the flag is set to indicate that the current register is now zero.
+*  @param	This function takes an integer value to write it in the reload regsister after decrementing it by one.
+*/
+
+
 void Systick_Wait(int delay)
 {
 	NVIC_ST_RELOAD_R = delay-1;
 	NVIC_ST_CURRENT_R = 0;
 	while((NVIC_ST_CTRL_R & 0x00010000)==0);
 }
+
+/**
+*  @author      Aser-ElDahshan
+*  @detailed    This fuction waites a multiple of a milli second.
+*  @param	The function takes an integer value representing time in milli seconds.
+*/
+
+
 void Systick_ms(int delay)// wait delay*1ms
 {
 	unsigned long i;
 	for( i=0;i<delay;i++)
 	Systick_Wait(16000);
 }
-void Systick_us(int delay)// wait delay*1us
-{
-	unsigned long i;
-	for( i=0;i<delay;i++)
-	Systick_Wait(16);
-}
+
 #endif
